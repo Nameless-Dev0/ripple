da051e872ff1695023c74ac2c6249d2605633608
Nameless-Dev0 <youssef.samir.mls@gmail.com>
2026-08-12 16:41:05 +0300

    sanity check for WAV parser functionality

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include "wav.h"
-#include "engine.h"
 
 int main(){
-    
+    wav_t* wav_in = load_wav("audio/song.wav");
+    wav_info(wav_in);
+    export_wav(wav_in);
+    release_wav(wav_in);
+
+    wav_t* wav_out = load_wav("audio/out.wav");
+    wav_info(wav_out);
+    release_wav(wav_out);
 
     return 0;
 }
