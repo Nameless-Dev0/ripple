1dcfc5222c80f876f8d50d21256fa9185c245e23
Nameless-Dev0 <youssef.samir.mls@gmail.com>
2026-08-12 21:56:14 +0300

    sanity check is_valid function

--- src/main.c
+++ src/main.c
@@ -1,12 +1,22 @@
+#include <stdio.h>
 #include "wav.h"
 
 int main(){
     wav_t* wav_in = load_wav("audio/song.wav");
+
+    if(is_valid_wav(wav_in))
+        printf("VALID WAV!\n");
+    else
+        printf("INVALID WAV!\n");
     wav_info(wav_in);
-    export_wav(wav_in);
+    export_wav(wav_in, "audio/out.wav");
     release_wav(wav_in);
 
-    wav_t* wav_out = load_wav("audio/out.wav");
+    wav_t* wav_out = load_wav("audio/exports/out.wav");
+    if(is_valid_wav(wav_out))
+        printf("VALID WAV!\n");
+    else
+        printf("INVALID WAV!\n");
     wav_info(wav_out);
     release_wav(wav_out);
 
